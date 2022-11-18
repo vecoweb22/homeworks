84454530a3f375b1891e2a41a8e9963d42ee7fc3
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2022-11-18 16:05:20 +0300

    Добавил решение

--- task_01.py
+++ task_01.py
@@ -10,3 +10,36 @@ print(my_favorite_songs[0:14])
 print(my_favorite_songs[-13:])
 print(my_favorite_songs[16:30])
 print(my_favorite_songs[-26:-15])
+
+# Да, ваше решение рабочее, но мы могли бы его улучшить. 
+# Например, не вручную посчитать индекс строки, а попробовать найти индекс через метод для строк find и rfind
+
+# Решение с помощью индекции строк
+
+first_song = my_favorite_songs [
+    : my_favorite_songs.find(',')
+]
+
+last_song = my_favorite_songs [
+    my_favorite_songs.rfind('A') :
+]
+
+second_song = my_favorite_songs[
+     my_favorite_songs.find('N') : 
+     my_favorite_songs.find(', S')
+]
+
+previous_song = my_favorite_songs [
+    my_favorite_songs.rfind('O') : my_favorite_songs.rfind(',')
+    ]
+
+print(first_song, last_song, second_song, previous_song)
+
+
+# Второй вариант, мы можем воспользоваться методом разделения строк по символам. split.
+# Полученный в результате список проиндексируем по песням
+
+# Решение с помощью метода split() и индексации списков
+songs = my_favorite_songs.split(', ')
+
+print(songs[0], songs[-1], songs[1], songs[-2])
